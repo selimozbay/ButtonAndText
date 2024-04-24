8c7db1a9aee6cef2ccf06dba8b20f131e49a2e69
Selim Özbay <selimozbay@yandex.com>
2024-04-25 00:54:27 +0300

    İçerik toplandı
    Tuşa basıldığında değişecek içerik listleri oluşturuldu.

---
+++ Assets/Scripts/generalScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class buttonScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI baslik, tur, icerik;
+
+    public string[] basliklar = 
+    {
+        "engineer",
+        "mother",
+        "home",
+        "programmer",
+        "firefighter",
+        "believe",
+        "create"
+        
+    };
+    public string[] turler =
+    {
+        "noun",
+        "noun",
+        "noun",
+        "noun",
+        "noun",
+        "verb",
+        "verb"
+        
+    };
+    public string[] icerikler =
+    {
+        "one who gets excited about things no one else cares about.",
+        "the person who sees the best in you no matter what.",
+        "the house, apartment, etc. where you live, especially with your family.",
+        "someone who fixes a problem you did not know you had in a way you do not understand.",
+        "someone who is fearsless in saving lives, decisive, courageous and determined, a true hero.",
+        "to have confidence or faith in the truth existence, reliability or value of something.",
+        "to evolve from one's own tought or imaginatiıns, as a work of art or an invention."
+
+        
+    };
+    void Start() {}
+
+public void Click() {
+    
+}
+}
